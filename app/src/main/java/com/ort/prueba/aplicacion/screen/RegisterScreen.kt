295a9a98ed1976eee2bdcfedcf8ff095ba0ae51d
MaraiNicolas <nicolasmarai@hotmail.com>
2025-09-01 21:29:39 -0300

    Commit Challenge 1 commit final

--- app/src/main/java/com/ort/prueba/aplicacion/screen/RegisterScreen.kt
+++ app/src/main/java/com/ort/prueba/aplicacion/screen/RegisterScreen.kt
@@ -1,38 +1,59 @@
 package com.ort.prueba.aplicacion.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.ort.prueba.aplicacion.components.AppButton
+import com.ort.prueba.aplicacion.components.AppCampoTexto
+import com.ort.prueba.aplicacion.components.AppCampoTextoTitulo
+import com.ort.prueba.aplicacion.components.AppLabelTexto
+import com.ort.prueba.aplicacion.components.SocialButton
+import com.ort.prueba.aplicacion.components.SocialButtons
 
 @Composable
 fun RegisterScreen(modifier: Modifier){
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .background(Color.White), // margen general
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-        Text(
+        AppCampoTextoTitulo(
             "Create Account",
             modifier = Modifier)
-        Text(
+        AppLabelTexto(
             "Create an account so you can explore all the existing jobs",
             modifier = Modifier
         )
-        OutlinedTextField(value = "", onValueChange = {}, label = { Text("Email") })
+        AppCampoTexto(label ="Email")
         Spacer(Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {}, label = { Text("Password") })
+        AppCampoTexto("Password")
         Spacer(Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {}, label = { Text("Confirm Password") })
+        AppCampoTexto("Confirm Password")
         Spacer(Modifier.height(16.dp))
-        Button(onClick = { /* TODO: lógica de registro */ }) { Text("Registrar")}
+        AppButton(text = "Sign Up",
+            onClick = { /* TODO: login logic */ });
         Spacer(Modifier.height(16.dp))
-        Button(onClick = { /* TODO: lógica de registro */ }) { Text("Create an account")}
+        AppLabelTexto("Create an account")
+
+        AppLabelTexto(
+            label = "Or continue with",
+            modifier = Modifier.height(10.dp)
+        )
+        SocialButtons();
     }
 }
--- app/src/main/java/com/ort/prueba/aplicacion/screen/WelcomeScreen.kt
+++ app/src/main/java/com/ort/prueba/aplicacion/screen/WelcomeScreen.kt
@@ -1,43 +1,52 @@
 package com.ort.prueba.aplicacion.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.ort.prueba.aplicacion.R
+import com.ort.prueba.aplicacion.components.AppCampoTexto
+import com.ort.prueba.aplicacion.components.AppCampoTextoTitulo
+import com.ort.prueba.aplicacion.components.AppLabelTexto
+import com.ort.prueba.aplicacion.components.LoginRegisterButtons
 
 @Composable
 fun WelcomeScreen(modifier : Modifier){
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .background(Color.White), // margen general
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
         Image(modifier = Modifier,
             painter = painterResource(  id= R.drawable.welcome_image),
             contentDescription = "Welcome",
         )
-        Text(
+        AppCampoTextoTitulo(
             "Discover Your",
-            modifier = Modifier,)
-        Text(
+            modifier = Modifier)
+        AppCampoTextoTitulo(
             "Dream Job here",
             modifier = Modifier
         )
-        Row(modifier = Modifier.fillMaxSize(),
-            ){
-            Button(onClick = { /* TODO: login logic */ }) {
-                Text("Login")
-            }
-            Button(onClick = { /* TODO: login logic */ }) {
-                Text("Register")
-            }
-        }
+        AppLabelTexto(label="Explore all the existing job roles based on your interest and study major")
+        Spacer(Modifier.height(50.dp))
+        LoginRegisterButtons()
 
     }
 }
